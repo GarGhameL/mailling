9743404192a100e680f0818ad4046b55b81f75f2
Finrot15 <s.gatica.l@gmail.com>
2023-12-26 22:58:31 -0300

    se agrega proyecto

---
+++ src/main/java/com/mycompany/eva2a/EVA2A.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2a;
+
+/**
+ *
+ * @author Finrot15
+ */
+public class EVA2A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
